ca78d24099bbbb9be9f0b025803f33bc8795b8cd
BR105 <brbruno1055@gmail.com>
2024-04-29 20:27:49 -0600

    Para que no se vea tan vacio

--- index.php
+++ index.php
@@ -38,6 +38,33 @@ session_start();
       </div>
     </div>
   </div>
+    
+    <div class="container-fluid">
+    <div class="row justify-content-center align-items-center">
+        <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false" data-bs-interval="false">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="assets/Logos/ds.jpg" class="d-block img-fluid mx-auto" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/Logos/ds1.jpg" class="d-block img-fluid mx-auto" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/Logos/ds3.jpeg" class="d-block img-fluid mx-auto" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+</div>
+
 
     <!-- Modal -->
     <div class="modal fade" id="modalIniciarSesion" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
